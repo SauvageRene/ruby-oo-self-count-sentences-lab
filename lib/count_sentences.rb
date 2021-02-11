e06b0da0103c6e2f87e9d4195d58b6565d3ff831
Rene <renegarcia1864@gmail.com>
2021-02-11 15:35:39 -0600

    commit tree 1de2182d0f2a0f9fe91e786087e67439a7ad7393

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,21 +1,61 @@
-require 'pry'
-
+ require 'pry'
 class String
-
-  def sentence? #returns true 
-      self.end_with?(".")
+  
+  def sentence?
+    if self.end_with?(".")
+    true
+    else
+      false
+    end
   end
 
   def question?
-      self.end_with?("?")
+    self.end_with?("?")
   end
 
   def exclamation?
     self.end_with?("!")
-
   end
 
   def count_sentences
     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class String
+
+#   def sentence? #returns true 
+#       self.end_with?(".")
+#   end
+
+#   def question?
+#       self.end_with?("?")
+#   end
+
+#   def exclamation?
+#     self.end_with?("!")
+
+#   end
+
+#   def count_sentences
+#     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+#   end
+# end
